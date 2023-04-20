1cab470947dddafe6c20533775263f83f22105ed
RockEnot22 <enotikav@gmail.com>
2023-04-20 13:25:21 +0100

    header and skills sections

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import HeaderSocial from "./HeaderSocials";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h3>Hello I'm</h3>
+        <h1>Viktoriia Mokhna</h1>
+        <h3 className="text-light">Junior front-end developer</h3>
+        <CTA />
+        <HeaderSocial />
+
+        <div className="me"></div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./nav.css";
+import {TbHome2} from "react-icons/tb";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <a className="nav__icon-link" href="#"><TbHome2 /></a>
+    </nav>
+  )
+}
+
+export default Nav
